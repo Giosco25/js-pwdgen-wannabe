5f6246f3d78a78fe18a227ba8a933ca2d10f9438
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-04-17 15:40:19 +0200

    myscript

---
+++ myscript.js
@@ -0,0 +1,11 @@
+//Chiedere all'utente il suo nome
+var nome_utente = prompt ('Digita il tuo nome');
+console.log(nome_utente);
+//Chiedere all'utente il suo cognome
+var cognome_utente = prompt('Digita il tuo cognome');
+console.log(cognome_utente);
+//Chiedere all'utente il suo colore preferito
+var colore_preferito = prompt('Digita il tuo colore preferito');
+console.log(colore_preferito);
+//Scrivere sulla pagina nome-cognome-colore-20
+document.writeln('La tua password è': nome_utente + cognome_utente + colore_preferito);
